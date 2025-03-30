6a6361dd9fe20a74c8426cad2a486c1a30cc1081
Guilhermeewe <guilhermesarnaglia2@gmail.com>
2025-03-29 23:04:33 -0300

    feat(repo) index.js

--- src/Pages/Repositorio/index.js
+++ src/Pages/Repositorio/index.js
@@ -1,6 +1,73 @@
+import { Container, Owner, Loading, BackButton, IssueList } from "./styles"
+import { useEffect, useState } from "react"
+import api from "../../services/api"
+import { useParams } from "react-router-dom"
+import { FaArrowLeft } from "react-icons/fa"
+
 const Repositorio = () => {
+    const { repositorio } = useParams()
+    const [repositorioData, setRepositorioData] = useState({})
+    const [issuesData, setIssuesData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+
+    useEffect(() => {
+        async function load() {
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${repositorio}`),
+                api.get(`/repos/${repositorio}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5,
+                        page: 1
+                    }
+                })
+            ])
+            setRepositorioData(repositorioData.data)
+            setIssuesData(issuesData.data)
+            setLoading(false)
+        }
+        load()
+    }, [])
+
+
     return (
-        <div style={{ color: "white" }}>Repositorio123</div>
+        <Container>
+            <BackButton to={'/'}>
+                <FaArrowLeft size={35} color="black" />
+            </BackButton>
+            <Owner>
+                <img src={repositorioData.owner?.avatar_url} alt="avatar" />
+                <h1>{repositorioData.name}</h1>
+                <p>{repositorioData.description}</p>
+                <p>{repositorioData.language}</p>
+            </Owner>
+
+            <IssueList>
+                {issuesData.map((issues) => {
+                    return (
+                        <li key={String(issues.id)}>
+                            <img src={issues.user.avatar_url} alt="avatarurl" />
+                            <div>
+                                <strong>
+                                    <a href={issues.html_url}>{issues.title}</a>
+                                    {
+                                        issues.labels.map((label) => {
+                                            return (
+                                                <span key={String(label.id)}>{label.name}</span>
+                                            )
+                                        })
+                                    }
+                                </strong>
+                                <p>{issues.user.login}</p>
+                            </div>
+                        </li>
+
+                    )
+                })}
+            </IssueList>
+        </Container>
     )
 }
 export default Repositorio
